66a34d0bdd5dda4cfa93b690e0f74acce6c0b64b
amaralc <amaral.calil@gmail.com>
2020-03-15 19:47:13 -0300

    API-CLIENT / FEATURE / Filtra pixels a partir de determinado threshold value

--- public/sketch.js
+++ public/sketch.js
@@ -1,15 +1,22 @@
 var video;
+var snapshot;
 
 function setup() {
-	createCanvas(320,240);
-	background(51);
 	video = createCapture(VIDEO);
 	video.size(320,240);
-	video.hide();	
+	video.hide();
+	createCanvas(320,240);
+	background(51);		
 }
 
 function draw() {	
 	image(video,0,0);
-	video.loadPixels();
-	console.log(video.pixels.length);
+	filter(THRESHOLD, 0.3);	
+
+	/**
+	 * Load pixels and log pixel length
+	 * video.loadPixels();
+	 * console.log(video.pixels.length);
+	 */
 }
+
