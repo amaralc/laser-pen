f1f5d227b58ea0c3c99bbfc13801c11fb59dd8b1
amaralc <amaral.calil@gmail.com>
2020-03-16 22:48:34 -0300

    FE-CLIENT / FEATURE / Create rgbFilter function with custom threshold values

--- public/sketch.js
+++ public/sketch.js
@@ -1,15 +1,21 @@
+// Declara variavel para receber objeto video
 var video;
-var snapshot;
 
 function setup() {
-	video = createCapture(VIDEO);
-	video.size(320,240);
-	video.hide();
+  // Cria objeto video
+  video = createCapture(VIDEO);
+
+  // Define tamanho do objeto video
+  video.size(320,240);
+
+  // Esconde objeto video
+  video.hide();
+
+  // Cria canvas
 	createCanvas(320,240);
-	background(51);
 }
 
-function draw() {
+function rgbFilter(video, rThreshold, gThreshold, bThreshold){
 
   // Carrega array de pixels
   video.loadPixels();
@@ -17,17 +23,25 @@ function draw() {
   // Varre array de pixels [R, G, B, A, R, G, B, A, ...]
   for (var i = 0; i < video.pixels.length; i += 4) {
 
-    // Edita R
-    if (video.pixels[i] < 200){
-      video.pixels[i] *= 0;
-    };
-    // Edita G
-    video.pixels[i + 1] *= 0;
-    // Edita B
-    video.pixels[i + 2] *= 0;
+    // Edita R < rThreshold
+    if (video.pixels[i] < rThreshold){video.pixels[i] *= 0};
+    // Edita G < gThreshold
+    if (video.pixels[i+1] < gThreshold){video.pixels[i+1] *= 0};
+    // Edita B < bThreshold
+    if (video.pixels[i+2] < bThreshold){video.pixels[i+2] *= 0};
+
   }
 
+  // Atualiza pixels
   video.updatePixels();
+
+  // Retorna video atualizado
+  return video;
+}
+
+function draw() {
+
+  video = rgbFilter(video, 250, 256, 256)
 	image(video,0,0);
 
 }
